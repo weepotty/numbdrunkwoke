d8bcecfee3a83c17b7968e5c3e0910694c3f228a
shona <shona@Hardy-Chans-MacBook-Pro.local>
2022-02-13 10:20:20 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+<script>
+function myFunction() {
+  var x = document.getElementByClass("hamburger");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+</script>
